68ecc77e647693b61eb995e52cfa4fed1b7a4c23
Mike <mbrie041@gmail.com>
2021-04-30 13:46:47 +0000

    Feature: About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  
+  resources :about, only: [:index]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
