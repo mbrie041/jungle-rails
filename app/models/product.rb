6ef73db3594478873aa3aa589f5684217c270a13
Mike <mbrie041@gmail.com>
2021-04-30 00:39:01 +0000

    UI Change: Order Details

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :line_items
 
   validates :name, presence: true
   validates :price, presence: true
