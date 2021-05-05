8bcf398b4455697caff9bc18262ed3b456e2fd3d
Mike <mbrie041@gmail.com>
2021-05-05 01:32:16 +0000

    add new authentication class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 6}
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @email = email.strip.downcase
+    @user = User.find_by_email(@email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,4 +74,39 @@ RSpec.describe User, type: :model do
       puts @user.errors.full_messages
     end
   end
+
+  describe '.authenticate_with_credentials' do
+    it "Should authenticate correct email with correct email" do
+      @user = User.create(
+        first_name: "John",
+        last_name: "Doe",
+        email: "john@test.com",
+        password: "realtest",
+        password_confirmation: "realtest")
+      @user_authenticated = User.authenticate_with_credentials("john@test.com", "realtest")
+      expect(@user).to eql(@user_authenticated)
+    end
+
+    it "Should not authenticate correct email with incorrect email" do
+      @user = User.create(
+        first_name: "John",
+        last_name: "Doe",
+        email: nil,
+        password: "realtest",
+        password_confirmation: "realtest")
+      @user_authenticated = User.authenticate_with_credentials("john@test.com", "realtest")
+      expect(@user).to_not eql(@user_authenticated)
+    end
+
+    it "Should authenticate correct email with correct email regardless of spaces or capitals" do
+      @user = User.create(
+        first_name: "John",
+        last_name: "Doe",
+        email: "john@test.com",
+        password: "realtest",
+        password_confirmation: "realtest")
+      @user_authenticated = User.authenticate_with_credentials("   JOHN@test.com", "realtest")
+      expect(@user).to eql(@user_authenticated)
+    end
+  end
 end
